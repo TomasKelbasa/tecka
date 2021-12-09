cf55f8815eb94ed03c0c009656af327e3cbb57c7
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2021-12-09 10:29:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+TRESHOLD = 512
+DELAY = 1000
+valueX = 2
+valueY = 2
+replot(valueX, valueY)
+
 def on_forever():
-    pass
+    tiltDirection = input.acceleration(Dimension.X)
+    #tiltDirection je v rozmezí -1024 a +1024
+    # control.millis()
+    # abs(-1000) vrací 1000
+    # abs(1000) vrací 1000
+
 basic.forever(on_forever)
-bbb
+
+def replot(x: number, y: number):
+    basic.clear_screen()
+    led.plot(x, y)
