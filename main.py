881b43fa26e96893efa841d96498ee31ec036e80
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2021-12-09 11:35:57 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,13 +2,41 @@ TRESHOLD = 512
 DELAY = 1000
 valueX = 2
 valueY = 2
+nakloneno = False
+cas_od_naklopeni = 0
 replot(valueX, valueY)
 
 def on_forever():
+    global nakloneno, valueX, valueY, cas_od_naklopeni
     tiltDirection = input.acceleration(Dimension.X)
     #tiltDirection je v rozmezí -1024 a +1024
     # control.millis()
     # abs(-1000) vrací 1000
     # abs(1000) vrací 1000
+    if(abs(tiltDirection) > TRESHOLD):
+        if(not nakloneno):
+            cas_od_naklopeni = control.millis()
+        nakloneno = True
+        
+    else:
+        nakloneno = False
+
+    console.log_value("x", tiltDirection)
+    if(nakloneno):
+        if(tiltDirection > TRESHOLD):
+            if(valueX < 4 & control.millis() - cas_od_naklopeni > DELAY):
+                cas_od_naklopeni = control.millis()
+                replot(++valueX,valueY)
+        if(tiltDirection < TRESHOLD):
+            if(valueX > 0 & control.millis() - cas_od_naklopeni > DELAY):
+                cas_od_naklopeni = control.millis()
+                replot(--valueX,valueY)
+
+            
 
 basic.forever(on_forever)
+
+def replot(x: number, y: number):
+    basic.clear_screen()
+    led.plot(x, y)
+
--- main.ts
+++ main.ts
@@ -1,3 +1,50 @@
+let TRESHOLD = 512
+let DELAY = 1000
+let valueX = 2
+let valueY = 2
+let nakloneno = false
+let cas_od_naklopeni = 0
+replot(valueX, valueY)
 basic.forever(function on_forever() {
     
+    let tiltDirection = input.acceleration(Dimension.X)
+    // tiltDirection je v rozmezí -1024 a +1024
+    //  control.millis()
+    //  abs(-1000) vrací 1000
+    //  abs(1000) vrací 1000
+    if (Math.abs(tiltDirection) > TRESHOLD) {
+        if (!nakloneno) {
+            cas_od_naklopeni = control.millis()
+        }
+        
+        nakloneno = true
+    } else {
+        nakloneno = false
+    }
+    
+    console.logValue("x", tiltDirection)
+    if (nakloneno) {
+        if (tiltDirection > TRESHOLD) {
+            if (valueX < (4 & control.millis() - cas_od_naklopeni) && (4 & control.millis() - cas_od_naklopeni) > DELAY) {
+                cas_od_naklopeni = control.millis()
+                replot(++valueX, valueY)
+            }
+            
+        }
+        
+        if (tiltDirection < TRESHOLD) {
+            if (valueX > (0 & control.millis() - cas_od_naklopeni) && (0 & control.millis() - cas_od_naklopeni) > DELAY) {
+                cas_od_naklopeni = control.millis()
+                replot(--valueX, valueY)
+            }
+            
+        }
+        
+    }
+    
 })
+function replot(x: number, y: number) {
+    basic.clearScreen()
+    led.plot(x, y)
+}
+
